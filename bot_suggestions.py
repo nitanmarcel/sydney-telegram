9e34edca85bf1a32a4e9024112dbec78b6b71095
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2023-03-28 02:13:20 +0300

    Fix suggestions only showing words

--- bot_suggestions.py
+++ bot_suggestions.py
@@ -6,12 +6,20 @@ async def get_suggestions(query):
     if not bot_config.SUGGESTIONS_API_KEY:
         return None
     suggestions = []
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f'https://www.bingapis.com/api/v7/suggestions?appid={bot_config.SUGGESTIONS_API_KEY}&q={query}') as response:
+    headers = {
+        'Accept-Language': 'en-US,en;q=1',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/111.0 BingSapphire/24.1.410310303'
+    }
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(f'https://www.bingapis.com/api/v7/suggestions?appid={bot_config.SUGGESTIONS_API_KEY}&etmkt=en-US&q={query}&lang=en-US,en;q=0.9') as response:
             if response.status != 200:
                 return None
             js = await response.json()
             for suggestionGroups in js['suggestionGroups']:
                 for searchSuggestion in suggestionGroups['searchSuggestions']:
-                    suggestions.append({'id': str(uuid.uuid4()), 'query': searchSuggestion['query']})
+                    if 'ghostText' in searchSuggestion.keys():
+                        suggestions.append({'id': str(uuid.uuid4()), 'query': searchSuggestion['ghostText']})
+                    else:
+                        suggestion = query + ' ' + searchSuggestion['query'].split(maxsplit=1)[-1]
+                        suggestions.append({'id': str(uuid.uuid4()), 'query': suggestion})
     return suggestions
