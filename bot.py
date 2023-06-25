1aa6d87af5103b7d72291c77038cbd3659c4b7ac
Marcel Alexandru Nitan <nitan.marcel@protonmail.com>
2023-06-25 07:16:03 +0000

    Fix image drawing response

--- bot.py
+++ bot.py
@@ -209,9 +209,9 @@ async def answer_builder(userId=None, chatID=None, style=None, query=None, cooki
         if can_swipe_topics:
             buttons.append(
                 [Button.inline(text='New Topic', data='newtopic')])
-        return answer.answer, buttons or None, query, False
         if isinstance(answer, bot_chat.ResponseTypeImage):
             return answer.images, buttons or None, answer.caption, True
+        return answer.answer, buttons or None, query, False
     except bot_chat.ChatHubException as exc:
         if can_swipe_topics:
             buttons.append(
